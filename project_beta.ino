7eac962945288f2a1e46b89da4b46f8fd8ce18fc
mstfymrtc <mstfymrtci@gmail.com>
2018-11-25 22:51:34 +0300

    project finish with servo motor

--- project_beta.ino
+++ project_beta.ino
@@ -1,29 +1,37 @@
+#include <LiquidCrystal.h>
 #include<Servo.h>
 Servo motor; // define motor
-const int sensorMin = 0; // set sensor min value
-const int sensorMax = 1024; // set sensor max value
-void setup() {
-  Serial.begin(9600); 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2); // define lcd
 
-  motor.attach(3); // attach motor to pin 3
+const int sensorMin = 0; // sensor maksimum
+const int sensorMax = 1024; // sensor minimum
+
+void setup() {
+  Serial.begin(9600); // set connection speed 
+  lcd.begin(16, 2);
+  lcd.print("Hello Master");
+  delay(1000);
+  lcd.clear();
+  motor.attach(9); // attach motor to pin 3
+
+
 }
 void loop() {
-  int sensorReading = analogRead(A0); //read value from sensor
-  int range = map(sensorReading, sensorMin, sensorMax, 0, 2); //map value to range
+  int sensorReading = analogRead(A0);
+  int range = map(sensorReading, sensorMin, sensorMax, 0, 2);
   switch (range) {
     case 0:
-
+      lcd.print("Rainy");
+      delay(500);
       motor.write(180); //set motor angle to 180 degree
-      delay(300);
+      lcd.clear();
       break;
-
     case 1:
-
+      lcd.print("No rain");
+      delay(500);
       motor.write(0); // set motor angle to initial
-      delay(300);
-
-
+      lcd.clear();
       break;
-
   }
+  delay(1);
 }
